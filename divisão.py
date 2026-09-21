8cbb6488520144350e806646a17263b33b5fd16f
EDUARDO DE ALENCAR CRISTALINO <eduardo.cristalino@cs.udf.edu.br>
2026-09-21 10:00:27 -0300

    feat: issue_divisão.py

---
+++ divisão.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite o primeiro numero a ser dividido: "))
+num2 = int(input("Digite o numero divisor: "))
+
+divi = num1/num2
+
+print(f"{num1} dividido por {num2} é igual a {divi}")
