288816ead919ceba1f713fdf31496a6bbc52728a
KAUAN DA SILVA MORAIS <kauan.morais@cs.udf.edu.br>
2026-09-21 10:27:14 -0300

    feat: multiplicação.py

---
+++ multiplicação.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite o primeiro número que deseja multiplicar:"))
+num2 = int(input("Digite o segundo número que deseja multiplicar:"))
+
+multiplicar = num1 * num2 
+
+print(f"O valor da multiplicação de {num1} e {num2} é {multiplicar}")
